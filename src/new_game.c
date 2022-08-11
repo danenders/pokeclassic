920989b44a21d503a442679e235d1312891bd13d
danenders <danenders@gmail.com>
2022-08-11 00:18:47 -0400

    Removed Truck Sequence. Spawn in Pallet Town.

--- src/new_game.c
+++ src/new_game.c
@@ -125,7 +125,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(PALLET_TOWN), MAP_NUM(PALLET_TOWN), WARP_ID_NONE, -1, -1);
     WarpIntoMap();
 }
 
