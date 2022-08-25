261ca7e6c87aea292af7bf1100db6099e9bf0687
danenders <danenders@gmail.com>
2022-08-25 16:20:10 -0400

    Updated a u8 in Battle Tower that causes an overflow in Multi Battles.

--- src/battle_tower.c
+++ src/battle_tower.c
@@ -1166,7 +1166,7 @@ void SetBattleFacilityTrainerGfxId(u16 trainerId, u8 tempVarId)
 {
     u32 i;
     u8 facilityClass;
-    u8 trainerObjectGfxId;
+    u16 trainerObjectGfxId;
 
     SetFacilityPtrsGetLevel();
     #ifndef FREE_BATTLE_TOWER_E_READER
