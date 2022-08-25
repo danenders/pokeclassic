8b8e4dbe3a02a783afa6cba11f059ddf986abadb
danenders <danenders@gmail.com>
2022-08-24 22:39:14 -0400

    Pokedex Area screen displays properly now.

--- src/pokedex_area_screen.c
+++ src/pokedex_area_screen.c
@@ -29,9 +29,9 @@
 //   determined by the data for the corresponding MAPSEC in gRegionMapEntries.
 
 // Only maps in the following map groups have their encounters considered for the area screen
-#define MAP_GROUP_TOWNS_AND_ROUTES MAP_GROUP(PETALBURG_CITY)
-#define MAP_GROUP_DUNGEONS MAP_GROUP(METEOR_FALLS_1F_1R)
-#define MAP_GROUP_SPECIAL_AREA MAP_GROUP(SAFARI_ZONE_NORTHWEST)
+#define MAP_GROUP_TOWNS_AND_ROUTES MAP_GROUP(PALLET_TOWN)
+#define MAP_GROUP_DUNGEONS MAP_GROUP(VICTORY_ROAD_1F)
+#define MAP_GROUP_SPECIAL_AREA MAP_GROUP(SAFARI_ZONE_CENTER)
 
 #define AREA_SCREEN_WIDTH 32
 #define AREA_SCREEN_HEIGHT 20
