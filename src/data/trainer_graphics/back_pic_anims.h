696b96557c9c54eaf1e660247f1f4fccefd85519
danenders <danenders@gmail.com>
2022-09-04 21:24:21 -0400

    Added Merp's Trainer Slide In

--- src/data/trainer_graphics/back_pic_anims.h
+++ src/data/trainer_graphics/back_pic_anims.h
@@ -18,6 +18,16 @@ static const union AnimCmd sAnimCmd_May_Steven_1[] =
     ANIMCMD_END,
 };
 
+static const union AnimCmd sAnimCmd_Point_HGSS[] =
+{
+    ANIMCMD_FRAME(3, 9),
+    ANIMCMD_FRAME(0, 9),
+    ANIMCMD_FRAME(2, 24),
+    ANIMCMD_FRAME(2, 24),
+    ANIMCMD_FRAME(3, 50),
+    ANIMCMD_END,
+};
+
 static const union AnimCmd sAnimCmd_Wally_1[] =
 {
     ANIMCMD_FRAME(0, 24),
@@ -45,6 +55,16 @@ static const union AnimCmd sAnimCmd_Leaf_1[] =
     ANIMCMD_FRAME(3, 6),
     ANIMCMD_FRAME(4, 24),
     ANIMCMD_FRAME(0, 1),
+    ANIMCMD_END,
+};
+
+static const union AnimCmd sAnimCmd_Point_HGSS_Red_Leaf[] =
+{
+    ANIMCMD_FRAME(0, 9),
+    ANIMCMD_FRAME(1, 9),
+    ANIMCMD_FRAME(3, 24),
+    ANIMCMD_FRAME(3, 24),
+    ANIMCMD_FRAME(0, 50),
     ANIMCMD_END,
 };
 
@@ -142,90 +162,105 @@ static const union AnimCmd *const sBackAnims_Brendan[] =
 {
     sAnim_GeneralFrame3,
     sAnimCmd_Brendan_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_May[] =
 {
     sAnim_GeneralFrame3,
     sAnimCmd_May_Steven_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_Red[] =
 {
     sAnim_GeneralFrame0,
     sAnimCmd_Red_1,
+    sAnimCmd_Point_HGSS_Red_Leaf,
 };
 
 static const union AnimCmd *const sBackAnims_Leaf[] =
 {
     sAnim_GeneralFrame0,
     sAnimCmd_Leaf_1,
+    sAnimCmd_Point_HGSS_Red_Leaf,
 };
 
 static const union AnimCmd *const sBackAnims_RubySapphireBrendan[] =
 {
     sAnim_GeneralFrame3,
     sAnimCmd_RubySapphireBrendan_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_RubySapphireMay[] =
 {
     sAnim_GeneralFrame3,
     sAnimCmd_RubySapphireMay_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_Wally[] =
 {
     sAnim_GeneralFrame3,
     sAnimCmd_Wally_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_Steven[] =
 {
     sAnim_GeneralFrame3,
     sAnimCmd_May_Steven_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_Ethan[] =
 {
     sAnim_GeneralFrame3,
     gAnimCmd_Ethan_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_Lyra[] =
 {
     sAnim_GeneralFrame3,
     gAnimCmd_Lyra_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_Kris[] =
 {
     sAnim_GeneralFrame3,
     gAnimCmd_Kris_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_Lucas[] =
 {
     sAnim_GeneralFrame3,
     gAnimCmd_Lucas_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_Dawn[] =
 {
     sAnim_GeneralFrame3,
     gAnimCmd_Dawn_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_DawnPlatinum[] =
 {
     sAnim_GeneralFrame3,
     gAnimCmd_DawnPlatinum_1,
+    sAnimCmd_Point_HGSS,
 };
 
 static const union AnimCmd *const sBackAnims_LucasPlatinum[] =
 {
     sAnim_GeneralFrame3,
     gAnimCmd_LucasPlatinum_1,
+    sAnimCmd_Point_HGSS,
 };
 
 const union AnimCmd *const *const gTrainerBackAnimsPtrTable[] =
