6709dc74a654ce9bbca704957bb0a48f4679c6b5
danenders <danenders@gmail.com>
2022-12-08 22:33:04 -0500

    Berries do not get removed

--- src/berry.c
+++ src/berry.c
@@ -1076,16 +1076,6 @@ static bool32 BerryTreeGrow(struct BerryTree *tree)
     case BERRY_STAGE_TALLER:
         tree->stage++;
         break;
-    case BERRY_STAGE_BERRIES:
-        tree->watered1 = 0;
-        tree->watered2 = 0;
-        tree->watered3 = 0;
-        tree->watered4 = 0;
-        tree->berryYield = 0;
-        tree->stage = BERRY_STAGE_SPROUTED;
-        if (++tree->regrowthCount == 10)
-            *tree = gBlankBerryTree;
-        break;
     }
     return TRUE;
 }
@@ -1099,7 +1089,7 @@ void BerryTreeTimeUpdate(s32 minutes)
     {
         tree = &gSaveBlock1Ptr->berryTrees[i];
 
-        if (tree->berry && tree->stage && !tree->stopGrowth)
+        if (tree->berry && tree->stage && !tree->stopGrowth && (tree->stage != BERRY_STAGE_BERRIES))
         {
             if (minutes >= GetStageDurationByBerryType(tree->berry) * 71)
             {
