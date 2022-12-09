522b4ace36edf07b5436e6374fe52a86e0e15e8f
danenders <danenders@gmail.com>
2022-12-08 21:51:56 -0500

    Ignore eggs at pokecenter

--- include/pokemon_storage_system.h
+++ include/pokemon_storage_system.h
@@ -71,6 +71,7 @@ void SetWaldaWallpaperColors(u16 color1, u16 color2);
 u8 *GetWaldaPhrasePtr(void);
 void SetWaldaPhrase(const u8 *src);
 bool32 IsWaldaPhraseEmpty(void);
+u8 CountPartyNonEggMons(void);
 
 void EnterPokeStorage(u8 boxOption);
 
--- src/field_effect.c
+++ src/field_effect.c
@@ -33,6 +33,7 @@
 #include "constants/rgb.h"
 #include "constants/songs.h"
 #include "event_data.h"
+#include "pokemon_storage_system.h"
 
 #define subsprite_table(ptr) {.subsprites = ptr, .subspriteCount = (sizeof ptr) / (sizeof(struct Subsprite))}
 
@@ -957,7 +958,7 @@ bool8 FldEff_PokecenterHeal(void)
     u8 nPokemon;
     struct Task *task;
 
-    nPokemon = CalculatePlayerPartyCount();
+    nPokemon = CountPartyNonEggMons();
     task = &gTasks[CreateTask(Task_PokecenterHeal, 0xFF)];
     task->tNumMons = nPokemon;
     task->tFirstBallX = 93;
